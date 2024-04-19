7cf7a2c63ed30910a5d2287e74da33c0093f4a6a
leejinwoo <leejinwoo1126@gmail.com>
2024-04-19 16:11:38 +0900

    feat: 4.9 Props 사용해보기

---
+++ src/components/AppProfile.jsx
@@ -0,0 +1,22 @@
+import '../App.css';
+import Profile from './Profile';
+
+function AppProfile() {
+  return (
+    <>
+      <Profile 
+        image='https://images.pexels.com/photos/14865644/pexels-photo-14865644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        name='tester1'
+        title='프론트 개발자'
+      />
+      <Profile 
+         image='https://images.pexels.com/photos/3238529/pexels-photo-3238529.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+         name='tester2'
+         title='백엔드 개발자'
+      />
+    </>
+  )
+  
+}
+
+export default AppProfile;
