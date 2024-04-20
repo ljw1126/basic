04f977b9586d732499313869583ea6bbc798d195
leejinwoo <leejinwoo1126@gmail.com>
2024-04-20 12:01:24 +0900

    feat: 5.4 마우스 따라가기 개선, 객체로 상태 관리

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,17 +2,19 @@ import React, {useState} from "react";
 import './AppXY.css';
 
 export default function AppXY() {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
+    const [position, setPosition] = useState({x: 0, y: 0});
 
     return (
         <div className="container" onPointerMove={(e) => {
-            setX(e.clientX);
-            setY(e.clientY);
+            setPosition({x: e.clientX, y: e.clientY});
+            // 수평으로만 이동 가능하다면
+            // setPosition(prev => ({x: e.clientX, y: prev.y}))
+            // 만약 z 축만 이동한다면 spread operator 사용해서 아래와 같이 필요한 부분만 재정의 가능
+            // setPosition(prev => ({prev.., z: e.clientZ}))
         }}>
             <div className='pointer'
                  style={{
-                     transform: `translate(${x}px, ${y}px)`
+                     transform: `translate(${position.x}px, ${position.y}px)`
                  }}
             />
         </div>
