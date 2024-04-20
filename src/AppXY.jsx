61f4392fe1cef1e8cf37c820b351853584f36155
leejinwoo <leejinwoo1126@gmail.com>
2024-04-20 12:26:04 +0900

    refactor: AppXY.jsx spread operator 수정

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -10,7 +10,7 @@ export default function AppXY() {
             // 수평으로만 이동 가능하다면
             // setPosition(prev => ({x: e.clientX, y: prev.y}))
             // 만약 z 축만 이동한다면 spread operator 사용해서 아래와 같이 필요한 부분만 재정의 가능
-            // setPosition(prev => ({prev.., z: e.clientZ}))
+            // setPosition(prev => ({...prev, z: e.clientZ}))
         }}>
             <div className='pointer'
                  style={{
