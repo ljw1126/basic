bbd227a31999f46830031aedf43075962a78f665
leejinwoo <leejinwoo1126@gmail.com>
2024-05-06 21:12:50 +0900

    feat: tailwind 설치, 예제 학습

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Button1 from './components/Button1'
 import Button2 from './components/Button2'
 import StyledComponents from "./StyledComponets";
+import TailwindComponent from "./TailwindComponent";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Button1/>
             <Button2/>
             <StyledComponents/>
+            <TailwindComponent/>
         </>
     );
 }
